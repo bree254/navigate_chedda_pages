1aa1206dac52ee5fae9be5edcbf0b0f4550a6adb
bree254 <brendawanjiru72@gmail.com>
2023-12-05 16:13:19 +0300

    changed ui

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,11 @@ class ChildrenPage extends StatelessWidget {
         itemBuilder: (context, index) {
           return GestureDetector(
             onTap: () {
-              String childAgeGroup = getChildAgeGroup(index);
-              navigateToAgeGroupPage(context, childAgeGroup);
+              String childName = getChildName(index);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ChildDashboard(childName)),
+              );
             },
             child: Container(
               color: Colors.blue,
@@ -52,121 +55,53 @@ class ChildrenPage extends StatelessWidget {
     );
   }
 
-  // Function to determine child's age group (replace with your logic)
-  String getChildAgeGroup(int index) {
-    if (index == 0) {
-      return 'AgeGroup1';
-    } else if (index == 1) {
-      return 'AgeGroup2';
-    } else {
-      return 'AgeGroup3';
-    }
-  }
-
-  // Function to navigate to age-specific pages based on age group
-  void navigateToAgeGroupPage(BuildContext context, String ageGroup) {
-    Widget pageToNavigate;
-
-    switch (ageGroup) {
-      case 'AgeGroup1':
-        pageToNavigate = AgeGroup1Page();
-        break;
-      case 'AgeGroup2':
-        pageToNavigate = AgeGroup2Page();
-        break;
-      case 'AgeGroup3':
-        pageToNavigate = AgeGroup3Page();
-        break;
-      default:
-        pageToNavigate = AgeGroup1Page(); // Default to AgeGroup1Page
-    }
-
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (_) => pageToNavigate),
-    );
+  // Function to determine child's name (replace with your logic)
+  String getChildName(int index) {
+    // You can replace this with your logic to get child's name
+    return 'Child $index';
   }
 }
 
-// Age Group 1 Page
-class AgeGroup1Page extends StatelessWidget {
+class ChildDashboard extends StatelessWidget {
+  final String childName;
+
+  ChildDashboard(this.childName);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Age Group 1'),
+        title: Text(childName),
       ),
       body: Center(
-        child: Text('Content for Age Group 1'),
+        child: Text('Dashboard for $childName'),
+        // Customize dashboard UI for each child...
       ),
-      bottomNavigationBar: CustomBottomNavBar(currentIndex: 0),
-    );
-  }
-}
-
-// Age Group 2 Page
-class AgeGroup2Page extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Age Group 2'),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_fire_department),
+            label: 'Quest',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: 'Wallet',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.emoji_people),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          // Handle bottom navigation item taps for each child
+          // Based on the index and child's data, navigate or perform actions
+        },
       ),
-      body: Center(
-        child: Text('Content for Age Group 2'),
-      ),
-      bottomNavigationBar: CustomBottomNavBar(currentIndex: 1),
-    );
-  }
-}
-
-// Age Group 3 Page
-class AgeGroup3Page extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Age Group 3'),
-      ),
-      body: Center(
-        child: Text('Content for Age Group 3'),
-      ),
-      bottomNavigationBar: CustomBottomNavBar(currentIndex: 2),
-    );
-  }
-}
-
-// Shared Bottom Navigation Bar Widget
-class CustomBottomNavBar extends StatelessWidget {
-  final int currentIndex;
-
-  CustomBottomNavBar({required this.currentIndex});
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: currentIndex,
-      type: BottomNavigationBarType.fixed,
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.local_fire_department),
-          label: 'Quest',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.account_balance_wallet),
-          label: 'Wallet',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.emoji_people),
-          label: 'Profile',
-        ),
-      ],
-      onTap: (index) {
-        // Handle bottom navigation item taps if needed
-      },
     );
   }
 }
